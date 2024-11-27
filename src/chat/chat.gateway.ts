6bcd7f789d79839eb931eb0e856c653307af8fde
gabriel berg <gbenberg@outlook.com>
2024-11-27 15:31:38 -0300

    course completed

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,22 +1,52 @@
-import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 import { ChatService } from './chat.service';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from 'src/users/interfaces/jwt-payload.interface';
 
 
 @WebSocketGateway({cors: true})
 export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer() wss: Server
 
-  constructor(private readonly chatService: ChatService) {}
+  constructor(
+    private readonly chatService: ChatService,
+    private readonly jwtService: JwtService
+  ) {}
 
-  handleConnection(client: Socket) {
-    this.chatService.registerClient(client)
+  async handleConnection(client: Socket) {
+
+    const token = client.handshake.headers.authentication as string
+    let payload: JwtPayload
+
+    try {
+      
+      payload = this.jwtService.verify(token)
+      await this.chatService.registerClient(client, payload.id)
+
+    } catch (error) {
+      client.disconnect()
+      return
+    }
 
     this.wss.emit('clients-updated', this.chatService.getConnectedClient())
   }
 
   handleDisconnect(client: Socket) {
     this.chatService.removeClient(client.id)
+    this.wss.emit('clients-updated', this.chatService.getConnectedClient())
+  }
+
+  @SubscribeMessage('message-from-client')
+  async onMessageFromClient(client: Socket, payload: any){
+
+    let jwtpayload: JwtPayload
+    
+    const token = client.handshake.headers.authentication as string
+    jwtpayload = this.jwtService.verify(token)
+
+    const fullName = await this.chatService.getUserName(jwtpayload.id)
+    this.wss.emit('message-from-server', {fullName, message: payload.message})
   }
 
   
--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -1,23 +1,62 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { InjectRepository } from '@nestjs/typeorm';
 import { Socket } from 'socket.io';
+import { User } from 'src/users/entities/user.entity';
+import { Repository } from 'typeorm';
 
 interface ConnectedClients{
-    [id: string]: Socket
+    [id: string]: {client: Socket, user: User}
 }
 
 @Injectable()
 export class ChatService {
     private connectedClients: ConnectedClients = {}
 
-    registerClient(client: Socket){
-        this.connectedClients[client.id] = client
-        console.log(this.connectedClients)
+    constructor(
+        @InjectRepository(User)
+        private readonly userRepository: Repository<User>,
+    ){}
+ 
+    async registerClient(client: Socket, id: string){
+        const user = await this.userRepository.findOneBy({id})
+        if(!user) throw new Error('not Found')
+        if(!user.isActive) throw new Error('not active user')
+        
+        await this.checkStatus(user) 
+
+        
+        this.connectedClients[client.id] = {client, user}
+    
     }
     removeClient(id: string){
         delete this.connectedClients[id]
     }
 
     getConnectedClient(): string[]{
-        return Object.keys(this.connectedClients)
+        let result: string[] = []
+        for(const client of Object.keys(this.connectedClients)){
+            const connectedClient = this.connectedClients[client]
+            result.push(connectedClient.user.fullname)
+        }
+
+        return result
+    }
+
+    async getUserName(id: string){
+        const { fullname } = await this.userRepository.findOneBy({id})
+        return fullname
+    }
+
+    private checkStatus(user: User){
+        for (const clientID of Object.keys(this.connectedClients)) {
+            const connectedClients = this.connectedClients[clientID]
+
+            if(connectedClients.user.id === user.id){
+                connectedClients.client.disconnect(); 
+                break;
+            }
+            
+        }
     }
 }
